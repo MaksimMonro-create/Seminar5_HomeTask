90384ddd543787cf2b09fc1d670ce4e7efeb2423
MaksimMonro-create <maksim.monro@yandex.ru>
2022-09-10 07:58:06 +0300

    Task3

---
+++ Task2/Program.cs
@@ -0,0 +1,48 @@
+﻿// НАПИСАТЬ ПРОГРАММУ МАСШТАБИРОВАНИЯ ФИГУРЫ
+
+// МАСШТАБИРОВАНИЕ РАВНОСТОРОННЕГО ЧЕТЫРЕХУГОЛЬНИКА
+
+
+int[,] GetNewCords(int[] a, int[] b, int[] c, int[] d, int k) 
+{
+    int[,] coor = new int[4, 2];                                
+    for (int ii = 0; ii < 2; ii++)                                  
+    {
+        for (int j = 0; j < 2; j++)                                 
+        {
+            if (ii == j)                                            
+            {
+                coor[0, j] = a[ii];                                
+                coor[1, j] = (b[ii] - a[ii]) * k + a[ii];
+                coor[2, j] = (c[ii] - a[ii]) * k + a[ii];
+                coor[3, j] = (d[ii] - a[ii]) * k + a[ii];
+            }
+        }
+    }
+    return coor;
+}
+
+void PrintCords(int[,] arr, char[] point)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        Console.Write($"{point[i]} ");
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            if (j == 0) Console.Write($"({arr[i, j]}, ");
+            else Console.WriteLine($"{arr[i, j]})");
+        }
+    }
+}
+
+int k = 2;                   // коэффициент масштабирования
+int[] pointA = { 1, 1 };     // координаты точек
+int[] pointB = { 2, 3 };     // координаты точек
+int[] pointC = { 5, 3 };     // координаты точек
+int[] pointD = { 4, 1 };     // координаты точек
+char[] pointName = { 'A', 'B', 'C', 'D' };
+
+Console.WriteLine();
+Console.WriteLine("Масштабирование");
+int[,] newCords = GetNewCords(pointA, pointB, pointC, pointD, k);
+PrintCords(newCords, pointName);
---
+++ Task3/Program.cs
@@ -0,0 +1,43 @@
+﻿// НАПИСАТЬ ПРОГРАММУ КОПИРОВАНИЯ МАССИВА
+
+int[] GetArray(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = new Random().Next(min, max + 1);
+    }
+    return arr;
+}
+
+int[] CopyArray(int[] originArray)
+{
+    int[] copy = new int[originArray.Length];
+    for (int i = 0; i < originArray.Length; i++)
+    {
+        copy[i] = originArray[i];
+    }
+    return copy;
+}
+
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write(arr[i] + " " );
+    }
+}
+
+int size = 5;
+int min = 1;
+int max = 10;
+
+int[] origin = GetArray(size, min, max);
+Console.WriteLine("Исходный массив:");
+PrintArray(origin);
+Console.WriteLine();
+Console.WriteLine("Копия массива:");
+int[] copy = CopyArray(origin);
+Console.WriteLine();
+PrintArray(copy);
+
